be80d928fe8e67c68fa63b2dc0fe5be2fb8e1eb9
satavisha123 <86342023+satavisha123@users.noreply.github.com>
2021-11-10 15:57:54 +0530

    Update Section.vue

--- components/cmscontent/Section.vue
+++ components/cmscontent/Section.vue
@@ -3,14 +3,21 @@
 </template>
 
 <script>
-
+//import BestSellers from './Section/BestSellers';
+import FeaturedCategories from './Section/FeaturedCategories';
+//import InstagramGallery from './Section/InstagramGallery';
 import NewsletterSignup from './Section/NewsletterSignup';
 import PromoBanner from './Section/PromoBanner';
+import NewProb from './section/NewProb.vue'
 export default {
   components: {
-   
+    // bestSellers: BestSellers,
+     featuredCategories: FeaturedCategories,
+      newProb : NewProb,
+    // instagramGallery: InstagramGallery,
     newsletterSignup: NewsletterSignup,
     promoBanner: PromoBanner
+   
   },
   props: {
     section: {
