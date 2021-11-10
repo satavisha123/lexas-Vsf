c57f41b2e0d2df06dad091e56da3e092b470e296
satavisha123 <86342023+satavisha123@users.noreply.github.com>
2021-11-10 15:57:10 +0530

    Add files via upload

---
+++ components/cmscontent/section/NewProb.vue
@@ -0,0 +1,25 @@
+<template>
+  <NewItem
+    :title="section.title"
+    :description="section.subtitle"
+    :button-text="section.buttonText"
+    :image="section.backgroundImage.url"
+  />
+</template>
+
+<script>
+
+
+export default {
+  components: {
+   NewItem
+  },
+
+  props: {
+    section: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
