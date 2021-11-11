02d31597686218471625488b234f34cc78990541
satavisha123 <86342023+satavisha123@users.noreply.github.com>
2021-11-11 15:27:48 +0530

    Add files via upload

---
+++ components/cmscontent/section/CallToAction.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="Card">
+  <SfCard
+    :title="section.title"
+     :buttontext="section.buttonText"
+    :description="section.description"
+   
+     />
+</div>
+</template>
+
+<script>
+import SfCard from '@storefront-ui/vue';
+export default {
+  components: {
+    SfCard
+  },
+  props: {
+    section: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+<style>
+.Card{
+  background-color: aqua;
+}
+</style>
+
