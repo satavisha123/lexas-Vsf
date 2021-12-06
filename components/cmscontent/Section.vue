834f2d5f23a93d845f8d1e5c563897802ff7e132
satavisha123 <86342023+satavisha123@users.noreply.github.com>
2021-12-06 20:23:43 +0530

    Add files via upload

---
+++ components/cmscontent/Section.vue
@@ -0,0 +1,37 @@
+<template><div>
+
+  <component :is="section.type" :section="section" />
+  </div>
+</template>
+
+<script>
+//import BestSellers from './Section/BestSellers';
+//import FeaturedCategories from './Section/FeaturedCategories';
+//import CallToAction from './Section/CallToAction';
+//import NewsletterSignup from './Section/NewsletterSignup';
+//import PromoBanner from './Section/PromoBanner';
+//import NewProb from './Section/NewProb';
+import Category from './section/Category';
+import newBanner from './section/newBanner.vue'
+//import Homepage from './section/FeaturedCategories.vue'
+export default {
+  components: {
+     //bestSellers: BestSellers,
+     //featuredCategories: FeaturedCategories,
+      //newProb : NewProb,
+     //callToAction: CallToAction,
+    //newsletterSignup: NewsletterSignup,
+    //promoBanner: PromoBanner
+    //homepage:Homepage,
+    category:Category,
+    newbanner:newBanner
+   
+  },
+  props: {
+    section: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
---
+++ components/cmscontent/section/Category.vue
@@ -0,0 +1,62 @@
+<template>
+<div>
+<div class="Card">
+  <div class="Shop-By-Category">
+{{section.contentTitle}}
+
+  </div>
+<div class="gridforcategory">
+  <div v-for="(items,i) in section.items" :key="i.id" >
+   {{items.title}}
+
+  </div>
+</div>
+</div>
+
+  <SfBannerGrid 
+     />
+</div>
+</template>
+
+<script>
+import SfBannerGrid  from '@storefront-ui/vue';
+export default {
+  components: {
+    SfBannerGrid 
+  },
+  props: {
+    section: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+<style>
+.Card{
+  /* width: 1079px;
+  height: 600px; */
+  display: grid;
+  
+
+}
+
+.Shop-By-Category {
+  /* width: 268px; */
+  height: 43px;
+  margin: 5px 5px 8px 100px;
+  font-family: 'Poppins';
+  font-size: 30px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.53;
+  letter-spacing: normal;
+  /* text-align: center; */
+  color: #4d4d4f;
+}
+
+.gridforcategory{
+  display: grid;
+}
+</style>
