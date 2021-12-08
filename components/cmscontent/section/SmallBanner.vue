2ca61dfe3651e064f34fdbd75623191b80674243
satavisha123 <86342023+satavisha123@users.noreply.github.com>
2021-12-08 15:12:21 +0530

    Add files via upload

---
+++ components/cmscontent/section/SmallBanner.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="main-grid">
+    <div v-for="(banner, i) in section.banner" :key="i.id" class="for-banner">
+     <div> {{ banner.title }}</div>
+       <img v-bind:src="banner.image.url" alt="Girl in a
+      jacket" class="item-Image"  width: 310px;
+  height: 268px;
+ />
+    </div>
+    <SfAccordion />
+  </div>
+</template>
+
+<script>
+import { SfAccordion } from "@storefront-ui/vue";
+
+export default {
+  components: {
+    SfAccordion,
+  },
+
+  props: {
+    section: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap");
+
+.main-grid{
+  display: grid;
+}
+
+.for-banner{
+  display: grid;
+}
+
+
+
+</style>
