6c25572ce226662870a349f571495c665adbc293
satavisha123 <86342023+satavisha123@users.noreply.github.com>
2021-12-22 15:10:22 +0530

    Add files via upload

---
+++ components/cmscontent/section/FooterNew.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ section }}
+    <SfBar></SfBar>
+  </div>
+</template>
+<script>
+import SfBar from "@storefront-ui/vue";
+export default {
+  components: {
+    SfBar,
+  },
+  props: {
+    section: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+<style>
+</style>
